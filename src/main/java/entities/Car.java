f2c4f8c06996120108f72dcdff71ed53a76d15be
Benjamin Bajrami <Aeydin24@gmail.com>
2019-11-19 11:45:29 +0100

    added import

--- src/main/java/entities/Car.java
+++ src/main/java/entities/Car.java
@@ -16,6 +16,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import entities.Location;
 
 /**
  *
